54dcf1fb96a9693dbaaa31c679e754646d3d1670
Duarte Peixinho <duarte.peixinho@gmail.com>
2021-07-17 20:44:43 +0100

    Added missing files

---
+++ src/php/Controllers/Default_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright This file is part of the framework developed by Peixinho
+ * @author Peixinho
+ */
+
+namespace FrameWork\Controller;
+use \FrameWork\Config\Internal\APP;
+
+require_once APP::ROOT_PATH()."/src/php/Views/Default_view.php";
+
+?>
