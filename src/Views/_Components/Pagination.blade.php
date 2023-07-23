f6d69dda076631f014fd3904afd4e5eb725e1c42
Duarte <duarte.peixinho@gmail.com>
2023-07-23 23:42:52 +0100

    Removed echo form pagination for debugging

--- src/Views/_Components/Pagination.blade.php
+++ src/Views/_Components/Pagination.blade.php
@@ -12,7 +12,6 @@
     @for ($i = 1; $i < $countPage + 1; $i++)
       @if ($i!=$current) <a href="?{{http_build_query(array_merge($_GET, array('page' => $i)))}}">{{$i}}</a> 
       @else <span>[ {{$i}} ]</span>
-      <a href="?{{http_build_query(array_merge($_GET, array('page' => $i)))}}">{{$i}}</a> 
       @endif
     @endfor
     @if (($current+1) <= $countPage) <a href="?{{http_build_query(array_merge($_GET, array('page' => ($current+1))))}}">&gt;&gt;</a> @endif
