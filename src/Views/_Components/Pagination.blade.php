6f8f18f182dc6c20013da667ae05f6a84a7c3ca7
Duarte Peixinho <peixinho@duarte.peixinho@gmail.com>
2025-04-28 23:48:56 +0100

    Updated styling in pagination component

--- src/Views/_Components/Pagination.blade.php
+++ src/Views/_Components/Pagination.blade.php
@@ -6,19 +6,30 @@
  */
  ?>
 
-@set($countPage = ($total/$limit))
+@set($countPage = ceil($total / $limit))
 @if ($countPage > 1)
 <div class="pagination">
-  @if ($current > 1) <span><a href="?{{http_build_query(array_merge($_GET, array('page' => ($current-1))))}}">&lt;&lt;</i></a></span>
-  @else <span>&lt;&lt;</span>
-  @endif
-  @for ($i = 1; $i < $countPage + 1; $i++)
-    @if ($i!=$current) <span><a href="?{{http_build_query(array_merge($_GET, array('page' => $i)))}}">{{$i}}</a></span>
-    @else <span>{{$i}}</span>
+    <!-- Previous Button -->
+    @if ($current > 1)
+        <span class="prev"><a href="?{{http_build_query(array_merge($_GET, ['page' => ($current-1)]))}}">&lt;&lt;</a></span>
+    @else
+        <span class="prev disabled" aria-disabled="true">&lt;&lt;</span>
     @endif
-  @endfor
-  @if (($current+1) <= $countPage) <a href="?{{http_build_query(array_merge($_GET, array('page' => ($current+1))))}}">&gt;&gt;</a></span>
-  @else <span>&gt;&gt;</span>
-  @endif
+
+    <!-- Page Numbers -->
+    @for ($i = 1; $i <= $countPage; $i++)
+        @if ($i != $current)
+            <span class="page"><a href="?{{http_build_query(array_merge($_GET, ['page' => $i]))}}">{{$i}}</a></span>
+        @else
+            <span class="page active" aria-current="page">{{$i}}</span>
+        @endif
+    @endfor
+
+    <!-- Next Button -->
+    @if (($current + 1) <= $countPage)
+        <span class="next"><a href="?{{http_build_query(array_merge($_GET, ['page' => ($current+1)]))}}">&gt;&gt;</a></span>
+    @else
+        <span class="next disabled" aria-disabled="true">&gt;&gt;</span>
+    @endif
 </div>
 @endif
