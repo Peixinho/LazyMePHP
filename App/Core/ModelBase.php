75937cb0fb392daa5a19c66a2e05ea940ac9e0ea
Duarte Peixinho <peixinho@duarte.peixinho@gmail.com>
2025-06-23 23:11:41 +0100

    Models are now working

---
+++ App/Core/ModelBase.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * LazyMePHP
+* @copyright This file is part of the LazyMePHP Framework developed by Duarte Peixinho
+* @author Duarte Peixinho
+*/
+
+declare(strict_types=1);
+
+namespace Core;
+
+class ModelBase
+{
+  protected bool $__initialized = false;
+
+  protected function initialize() : void {
+    $this->__initialized = true;
+  }
+
+  protected function isInitialized() : bool {
+    return $this->__initialized;
+  }
+
+}
