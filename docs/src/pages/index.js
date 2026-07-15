5f52060b9eb9cdbc7ce585c00dc26ea6a56c584f
Duarte Peixinho <peixinho@duarte.peixinho@gmail.com>
2026-07-15 21:10:35 +0100

    Fixed docs/src/pages/index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,5 +1,6 @@
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/intro" />;
+  return <Redirect to={useBaseUrl('/intro')} />;
 }
